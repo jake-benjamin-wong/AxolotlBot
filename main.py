1d914353aaeb4e2fa30505f7e2ef92b6925b17f4
jake-benjamin-wong <jake.benjamin.wong@gmail.com>
2021-07-05 16:34:56 +0800

    initial commit wow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import discord
 from dotenv import load_dotenv
+from urllib import parse, request
+import re
+import datetime
 
 from discord.ext import commands
 
@@ -10,24 +13,37 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 # Is this better?
-bot = commands.Bot(command_prefix='$', description="This is a test bot")
+bot = commands.Bot(command_prefix='!', description="This is a test bot")
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-
+    await bot.change_presence(activity=discord.Game("Use !help"))
 
 # commands here
-
 @bot.command(name='ping')
 async def ping(ctx):
     await ctx.send("pong")
 
+@bot.command()
+async def youtube(ctx, *, search,):
+    await ctx.send("finding video pls wait")
+    query_string = parse.urlencode({'search_query': search})
+    html_content = request.urlopen('http://www.youtube.com/results?' + query_string)
+    print(query_string)
+    #print(html_content.read().decode())
+    search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+    await ctx.send('https://www.youtube.com/watch?v=' + search_results[0])
 
 @bot.command()
-async def test(ctx, *args):
-    await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+async def clear(ctx, amount=10):
+    await ctx.channel.purge(limit=amount)
 
+@bot.listen()
+async def on_message(message):
+    if "die" in message.content.lower():
+        await message.channel.send("your ded lmao")
+        await bot.process_commands(message)
 
 bot.run(TOKEN)
